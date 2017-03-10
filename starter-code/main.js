63ee3b1232c16af4bf04bf6f919ad6bb58fe20d5
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-03-10 13:54:57 -0800

    created alert

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,11 @@ var cardOne = "Queen";
 var cardTwo = "Queen";
 var cardThree = "King";
 var cardFour = "King";
+
+if ("King" == "King"){
+	alert("You found a match!");
+} else if ("Queen" == "Queen"){
+	alert("You found a match!");
+} else if ("Queen" == "King"){
+	alert("Sorry, try again.")
+}
