15835b522efdbc477603c7aa6669b6c9ffa8b385
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-03-15 13:06:36 -0700

    created cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,24 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = {'Queen', 'Queen', 'King', 'King'};
+var cardsInPlay = [];
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
-if ("King" == "King"){
+/*if ("King" == "King"){
 	alert("You found a match!");
 } else if ("Queen" == "Queen"){
 	alert("You found a match!");
 } else if ("Queen" == "King"){
 	alert("Sorry, try again.")
+}*/
+
+document.getElementByID('game-board')
+
+var createCards = function (){
+	for(var i=0; i<cards.length; i++){
+
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		gameBoard.appendChild(cardElement);
+	}
 }
+
+createCards();
